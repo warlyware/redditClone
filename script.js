f50073cba4fef793615d2107468b9bf85729fc1e
warlyware <danward@gmail.com>
2015-06-25 12:34:26 -0700

    Add basic link adding/listing functionality

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 'use strict';
 
+console.log('angular loading');
 angular.module('heimdall', [])
-.controller('MainCtrl', function($scope){
+.controller('MainCtrl', function($scope, $http){
+  $scope.links = [];
+  $scope.addLink = function() {
+    $scope.links.push($scope.link);
+    $scope.link = {};
+  }
 });
